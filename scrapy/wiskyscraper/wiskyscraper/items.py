cf709eb2dc8d595873c3bf373c8b04ab8a236197
CharlesSwei <sweicharles@gmail.com>
2021-07-12 02:30:45 +1000

    Finish the whiskyspider and items using item loader

--- scrapy/wiskyscraper/wiskyscraper/items.py
+++ scrapy/wiskyscraper/wiskyscraper/items.py
@@ -4,11 +4,20 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader import ItemLoader
+from itemloaders.processors import TakeFirst, MapCompose
+from w3lib.html import remove_tags
+
+
+def remove_currency(value):
+    return value.replace('£', '').strip()
 
 
 class WiskyscraperItem(scrapy.Item):
     # define the fields for your item here like:
-    name = scrapy.Field()
-    price = scrapy.Field()
+
+    # The input_processor takes functions to deal with the data, and then pass to output processor 
+    name = scrapy.Field(input_processor = MapCompose(remove_tags), output_processor = TakeFirst())
+    price = scrapy.Field(input_processor = MapCompose(remove_tags, remove_currency), output_processor = TakeFirst())
     link = scrapy.Field()
 
--- scrapy/wiskyscraper/wiskyscraper/spiders/wiskyspider.py
+++ scrapy/wiskyscraper/wiskyscraper/spiders/wiskyspider.py
@@ -1,6 +1,7 @@
 import scrapy 
 # Import the item class for fetching data
 from wiskyscraper.items import WiskyscraperItem
+from scrapy.loader import ItemLoader
 
 class wiskySpider(scrapy.Spider):
     name = 'whisky'
@@ -14,20 +15,16 @@ class wiskySpider(scrapy.Spider):
         # get all the return item 
         for product in response.css('div.product-item-info'):
 
-            item = WiskyscraperItem()
+            # 1. selector ('product') select the item from the html and send it to itemloader
+            # 2. the item loader use wiskyscrapperItem() and send the data to item clean
+            # ==> means we create a item loader, this is my item and this is my selector, go and grab the data for me
+            l = ItemLoader(item = WiskyscraperItem(), selector = product)
+            
+            l.add_css('name', 'a.product-item-link')
+            l.add_css('price', 'span.price')
+            l.add_css('link', 'a.product-item-link::attr(href)' )
 
-
-
-            # normal funtion in python is return, but scrapy uses 'yield'
-            item['name'] = product.css('a.product-item-link::text').get()
-            try: 
-                item['price'] =  product.css('span.price::text').get().replace('£', ''),
-            except :
-                item['price']  = None
-
-            item['link'] =  product.css('a.product-item-link').attrib['href']
-
-            yield item
+            yield l.load_item()
 
         # find the next page button(element where link to next page), or a way to link to next page
         next_page = response.css('a.action.next').attrib['href']
